f81eaecff7647a4e040ddf7fb1272c512313e2a9
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-11-30 20:34:12 -0500

    Main SetUp

--- cli.py
+++ cli.py
@@ -1,39 +1,71 @@
 
-import logging
-from game import SantoriniGame, Move
+import sys
+from game import SantoriniGame, Command, Player
 
 class SantoriniCLI:
     """Command-line interface."""
 
-    def __init__(self):
-        self._santorini_game = SantoriniGame()
-        # we will maybe use it as a key to come back in the history
-        self._current_turn = 1
+    def __init__(self, args):
+        # HERE: Should we hold this config here?
+        self._white_player = args['white_player']
+        self._blue_player = args['blue_player']
+        self._undo_redo = args['undo_redo'] == "on"
+        self._score_display = args['score_display'] == "on"
+
+        # Create players and workers
+        white_workers = [("A", (3, 1)), ("B", (3, 2))]
+        blue_workers = [("Y", (1, 2)), ("Z", (1, 4))]
+        white = Player(self._white_player, white_workers)
+        blue = Player(self._blue_player, blue_workers)
+        players = [white, blue]
+
+        # Create the Game
+        self._game = SantoriniGame(players)        
 
     def run(self):
         """Infinite turn loop."""
-        while True:
-            print(f"Turn: {self._current_turn}, {self._santorini_game.get_current_player()} ({self._santorini_game.get_current_player_workers()})")
-            self._santorini_game.print_board()
-
+        while True: # change later
+            self._game.print_board()
             self._process_turn()
-
-            self._current_turn += 1
 
     def _process_turn(self):
         worker_to_move = input("Select a worker to move: ")
         direction_to_move = input("Select a direction to move (n, ne, e, se, s, sw, w, nw): ")
         direction_to_build = input("Select a direction to build (n, ne, e, se, s, sw, w, nw): ")
 
-        move = Move(worker_to_move, direction_to_move, direction_to_build)
-        self._santorini_game.execute_move(move)
+        command = Command()
+        # Think about Command Pattern
 
 
-if __name__ == "__main__":
-    try:
-        SantoriniCLI().run()
-    except KeyboardInterrupt:
-        print("\nGame terminated by the player.")
-    except Exception as e:
-        print("Sorry! Something unexpected happened. Check the logs or contact the developer for assistance.")
-        logging.error(str(e.__class__.__name__) + ": " + repr(str(e)))
+def parse_args():
+    args = sys.argv[1:]
+    white_player_type = args[0] if args and args[0] in ["human", "heuristic", "random"] else "human"
+    blue_player_type = args[1] if len(args) > 1 and args[1] in ["human", "heuristic", "random"] else "human"
+    undo_redo_enabled = args[2] if len(args) > 2 and args[2] in ["on", "off"] else "off"
+    score_display_enabled = args[3] if len(args) > 3 and args[3] in ["on", "off"] else "off"
+
+    return {
+        "white_player": white_player_type,
+        "blue_player": blue_player_type,
+        "undo_redo": undo_redo_enabled,
+        "score_display": score_display_enabled
+    }
+
+if __name__ == "__main__": 
+    args = parse_args()
+
+    # Create players and workers
+    white_workers = [("A", (3, 1)), ("B", (3, 2))]
+    blue_workers = [("Y", (1, 2)), ("Z", (1, 4))]
+
+    white_player = Player("white", white_workers)
+    blue_player = Player("blue", blue_workers)
+
+    players = [white_player, blue_player]
+
+    # Initialize the game & board with players
+    santorini_game = SantoriniGame(players)
+
+    # Run & display the game
+    cli = SantoriniCLI(args)
+    cli.run()
--- game.py
+++ game.py
@@ -1,31 +1,99 @@
+
+class Worker:
+    """Class representing a worker."""
+
+    def __init__(self, symbol, default_position):
+        self.symbol = symbol
+        self.position = default_position
+
+    @property
+    def get_symbol(self):
+        return self.symbol
+
+
+class Player:
+    """Class representing a player."""
+
+    def __init__(self, color, workers):
+        self.color = color
+        self.workers = workers
+
+    @property
+    def get_workers(self):
+        return self.workers
+    
+    @property
+    def get_color(self):
+        return self.color
+
+class WhitePlayer(Player):
+    """Class representing the White player."""
+
+    def __init__(self):
+        workers = [Worker('A', (3, 1)), Worker('B', (3, 3))]
+        super().__init__('white', workers)
+
+class BluePlayer(Player):
+    """Class representing the Blue player."""
+
+    def __init__(self):
+        workers = [Worker('Y', (1, 2)), Worker('Z', (1, 4))]
+        super().__init__('blue', workers)
+
+
+# Here Command Pattern
+class Command:
+    """Class representing a player move."""
+
+    def __init__(self):
+        pass
+
+
 class SantoriniGame:
     """Class representing the Santorini game."""
 
-    def __init__(self):
-        # Add your initialization logic here
-        pass
+    def __init__(self, players):
+        self._players = players
+        self._board = Board(self._players[0], players[1])
+        self._current = self._players[0]
+        self.turn = 1
 
     def print_board(self):
-        # Add logic to print the game board
+        print(f"Turn: {self.turn}, {self._current.get_color} ({[worker[0] for worker in self._current.get_workers]})")
+        self._board.print_board()
+
+    # Command Pattern Caller (Another Class)
+    def execute_command(self, command):
         pass
 
+    @property
     def get_current_player(self):
-        # Add logic to get the current player
-        pass
+        return self._currentxww
+    
 
-    def get_current_player_workers(self):
-        # Add logic to get the workers of the current player
-        pass
+class Board:
+    """Class representing the Santorini Board."""
 
-    def execute_move(self, move):
-        # Add logic to execute a move
-        pass
+    def __init__(self, white, blue):
+        self._grid = [[(0, []) for _ in range(5)] for _ in range(5)]
 
+        self._add_workers_to_cell(3, 1, white.get_workers[0])
+        self._add_workers_to_cell(1, 3, white.get_workers[1])
+        self._add_workers_to_cell(1, 1, blue.get_workers[0])
+        self._add_workers_to_cell(3, 3, blue.get_workers[1])
 
-class Move:
-    """Class representing a player move."""
+    def print_board(self):
+        """Print the board."""
+        for row in range(5):
+            print("+--+--+--+--+--+")
+            for col in range(5):
+                height, workers = self._grid[row][col]
+                print(f"|{height}{workers[0] if workers else ' '}", end="")
 
-    def __init__(self, worker, direction_to_move, direction_to_build):
-        self.worker = worker
-        self.direction_to_move = direction_to_move
-        self.direction_to_build = direction_to_build
+            print("|")
+        print("+--+--+--+--+--+")
+
+    def _add_workers_to_cell(self, row, col, workers):
+        """Add workers to the cell."""
+        self._grid[row][col] = (0, workers)
+
